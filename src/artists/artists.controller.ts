c5b73a45519803e1a9f767e8e9c9bf7400a2f671
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2022-04-03 13:01:48 +0600

    Get all artists.

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,15 +1,24 @@
 import { Controller, Get, Param, Post } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Artist, ArtistDocument } from '../schemas/artist.schema';
+import { Model } from 'mongoose';
 
 @Controller('artists')
 export class ArtistsController {
+  constructor(
+    @InjectModel(Artist.name) private artistsModel: Model<ArtistDocument>,
+  ) {}
+
   @Get()
   getAll() {
-    return { message: 'Hello' };
+    return this.artistsModel.find();
   }
+
   @Post()
   create() {
     return { message: 'Create artist' };
   }
+
   @Get(':id')
   getOne(@Param('id') id: string) {
     return { message: `Artist id: ${id}` };
