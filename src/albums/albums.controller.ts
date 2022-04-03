583c845acdc40eaea779fb735aec95b07733d4df
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2022-04-03 14:56:12 +0600

    Albums controller (get all, get one, create, remove)

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Album, AlbumDocument } from '../schemas/album.schema';
+import { Model } from 'mongoose';
+import { CreateAlbumDto } from './create-album.dto';
 
 @Controller('albums')
-export class AlbumsController {}
+export class AlbumsController {
+  constructor(
+    @InjectModel(Album.name) private albumModel: Model<AlbumDocument>,
+  ) {}
+
+  @Get()
+  getAll() {
+    return this.albumModel.find();
+  }
+
+  @Get(':id')
+  GetByArtist(@Param('id') id: string) {
+    return this.albumModel.findOne({ _id: id });
+  }
+
+  @Post()
+  async create(@Body() albumDto: CreateAlbumDto) {
+    const album = new this.albumModel({
+      title: albumDto.title,
+      artist_id: albumDto.artist_id,
+      year: albumDto.year,
+      image: albumDto.image,
+      isPublished: albumDto.isPublished,
+    });
+
+    await album.save();
+
+    return album;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.albumModel.findByIdAndRemove({ _id: id });
+  }
+}
---
+++ src/albums/create-album.dto.ts
@@ -0,0 +1,7 @@
+export class CreateAlbumDto {
+  title: string;
+  artist_id: string;
+  year: string;
+  image: string;
+  isPublished: boolean;
+}
